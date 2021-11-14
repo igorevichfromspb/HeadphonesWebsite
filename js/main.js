1e339a658f27bdd2160d2a20a5e3f0c977b91ad0
Ivan Matveev <igorevichwork@gmail.com>
2021-11-14 20:47:55 +0300

    create: media queries

--- js/main.js
+++ js/main.js
@@ -41,5 +41,25 @@ function scrollUp() {
 }
 window.addEventListener("scroll", scrollUp);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active__link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active__link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
